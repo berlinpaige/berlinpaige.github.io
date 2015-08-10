fd7bab905f1f406d9c4b0b71413a471b02b75eb3
Berlin Sohn <berlinsohn@gmail.com>
2015-08-10 13:15:29 -0500

    Adding Jquery functionality

---
+++ jquery.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('button').on('click', function(){
+     $('.tech, .health, .mission, .name, h1, h3, footer, button').css({'background-color':'#b2f0ff','color':'#000066'});
+ });
+});
